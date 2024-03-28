10b14b568c5d5fb508cc9b72871c1285007420a0
Soimu Alexandru <soimualex97@gmail.com>
2024-03-28 17:51:57 +0200

    Initial commit

---
+++ cypress/e2e/TesteLinkedIn/teste-linkedin.cy.js
@@ -0,0 +1,12 @@
+describe("On LinkedIn", () => {
+  it("I can login.", () => {
+    cy.visit("https://linkedin.com");
+
+    cy.get(".nav__button-secondary").click();
+    cy.get("#username").type("soimualex97@gmail.com");
+    cy.get("#password").type("Systemgenius0");
+    cy.get(".btn__primary--large").click();
+
+    cy.get("#ember18").should("exist");
+  });
+});
