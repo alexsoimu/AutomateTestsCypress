9ec8482f58176a9f2acbd18274022393b98a230f
Soimu Alexandru <soimualex97@gmail.com>
2024-04-23 13:24:58 +0300

    feat: credentials modified

--- cypress/e2e/TesteLinkedIn/teste-linkedin.cy.js
+++ cypress/e2e/TesteLinkedIn/teste-linkedin.cy.js
@@ -4,7 +4,7 @@ describe("On LinkedIn", () => {
 
     cy.get(".nav__button-secondary").click();
     cy.get("#username").type("soimualex97@gmail.com");
-    cy.get("#password").type("Systemgenius0");
+    cy.get("#password").type("*********");
     cy.get(".btn__primary--large").click();
 
     cy.get("#ember18").should("exist");
